a1139a25b81a98654b4ba204fb7ffaa3078dd521
a941884799 <941884799@qq.com>
2019-11-11 13:52:05 +0800

    11.11 13:40 修改头部2

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 export default header;
- function header(path) {
+function header(path) {
     //将公共头部引入页面
     $('#header').load(path, function () {// 引入头部后执行
         //控制二级导航栏的显隐
@@ -12,19 +12,20 @@ export default header;
             function () { $(this).find('.hide').css("display", "none") }
         );
 
-        $('#header .login').on('click', () => {//打开登录框
-            loginInit();//初始化登录框
+        //打开登录框
+        $('#header .login').on('click', () => {
+            $('#login').html(loginDom);//加载登录模块
+            loginInit();//初始化登录框状态
             $('#login').stop(true, true).fadeIn(400, () => { $('#header .mengBan').css("display", "block") });
         });
 
-        $('#login .del').on('click', () => {//点击×关闭登录框
-            $('#login').find('[style]').removeAttr('style');
-            $('#login .option').removeClass('active');//清除行内样式
-            clearInterval(timer);
-            $('#login').stop(true, true).fadeOut(400, () => { $('#header .mengBan').css("display", "none") });
+        //关闭登录框 
+        $('#login').on('click', '.del', ()=>{
+            clearInterval(timer);//清除计时器
+            delLogin();//清除登录模块
         });
 
-        $('#login .option').on('click', function () {// 选择登录方式
+        $('#login').on('click', '.option', function () {// 选择登录方式
             var index = $('#login .option').index(this);
             $(this).addClass('active');
             $(this).siblings('.option').removeClass('active');
@@ -32,27 +33,28 @@ export default header;
             $('#login .con').eq(index).siblings('.con').css('display', 'none');
         });
 
-        $('#login .selecter').on('click', function (e) {// 选择手机号前置号码
+        // 选择手机号前置号码
+        $('#login').on('click', '.selecter', function (e) {
             e.stopPropagation();
             $('#login .selecter .list').toggle();
             document.onclick = () =>
                 $('#login .selecter .list').css('display', 'none');
         });
-
-        $('#login .selecter .list').on('click', 'li', function (each) {
+        $('#login').on('click', '.selecter .list li', function (each) {
             $('#login .selecter .number').text($(this).find('b').text())
         });
 
-        var timeOut = null, iphoneNunber;
-        $('#login input[type=text]').on({//手机号码验证
-            "keydown": function () {
+        //手机号验证
+        var timeOut = null;
+        $('#login').on({
+            "input": function () {
                 var hint = $(this).siblings('.hint');
                 var reg = /^1\d{10}$/;
+                $('#login .hint2').css('display','none')
                 $(this).val($(this).val().trim());//清除首位空格
                 clearTimeout(timeOut);//防抖
                 timeOut = setTimeout(() => {
-                    iphoneNunber = $('#login .tx1').val();
-                    if (reg.test(iphoneNunber)) {//验证手机号是否合格
+                    if (reg.test($('#login .txt').val())) {//验证手机号是否合格
                         $(hint).text('✔');
                         $(hint).css("borderWidth", "0");
                         if ($('#login .bt1').val() == '获取验证码') {//防止多次激活
@@ -72,7 +74,7 @@ export default header;
                         $('#login .bt2').addClass('active2');
                 }, 550);
             }
-        });
+        }, 'input[type=text]');
         // 验证码
         $('#login').on('click', '.active1', e => {
             // 初始化验证框
@@ -83,43 +85,49 @@ export default header;
             var str = randomWord(5);
             $('#login .son .code').text(str);
         });
-        var bln = false, timer = null;//短信验证成功后为true
+
+        var bln = false, timer = null, iphoneNunber;//短信验证成功后为true
         $('#login').on('click', '.bt3', e => {//打开验证框
             var str1 = $('#login .code').text().toLowerCase();
             var str2 = $('#login .t3').val().toLowerCase();
             if (str1 === str2) {//验证成功
-                $('#login .son').fadeOut(100, () => {//关闭验证框
+                $('#login .son').fadeOut(500, () => {//关闭验证框
                     $('#header .mengBan2').css('display', 'none');
                     $('#login .t3').val('');
+                    //存储手机号
+                    iphoneNunber = $('#login .txt').val();
+                    // 发送短信
+                    var code = randomWord(6, 10);//6位数字验证码
+                    alert(code);//发送
+                    $('#login .bt1').removeClass('active1');
+                    var num = 60;
+                    $('#login .bt1').val(`重新发送${num}`);
+                    timer = setInterval(() => {//开始计时
+                        $('#login .bt1').val(`重新发送${--num}`);
+                        if (num <= 0) {//初始获取按钮
+                            $('#login .bt1').val('获取验证码');
+                            $('#login .bt1').addClass('active1');
+                            clearInterval(timer);
+                        };
+                        bln = ($('#login .t2').val() == code);//验证成功后为true
+                    }, 1000);
                 });
-                // 发送短信
-                var code = randomWord(6, 10);//6位数字验证码
-                alert(code);//发送
-                $('#login .bt1').removeClass('active1');
-                var num = 60;
-                $('#login .bt1').val(`重新发送${num}`);
-                timer = setInterval(() => {//开始计时
-                    $('#login .bt1').val(`重新发送${--num}`);
-                    if (num <= 0) {//初始获取按钮
-                        $('#login .bt1').val('获取验证码');
-                        $('#login .bt1').addClass('active1');
-                        clearInterval(timer);
-                    };
-                    bln = ($('#login .t2').val() == code);//验证成功后为true
-                }, 1000);
             } else {//验证错误
                 $('#login .son .hide').fadeIn(100, () => {//打开提示栏
                     setTimeout(() => { $('#login .son .hide').fadeOut(100); }, 3000)
                 });
             };
         });
+
         $('#login').on('click', '.del2', function () {//关闭验证框,关闭条款
             $(this).closest('.w-position').fadeOut(100, () => {
                 $('#header .mengBan2').css('display', 'none');
             });
         });
+
+        //判定手机验证码
         $('#login').on('click', '.bt2', e => {
-            if (bln) {
+            if (bln && ($('#login .txt').val() == iphoneNunber)) {
                 $('#login .clause').fadeIn(100, () => {//出现条款
                     $('#header .mengBan2').css('display', 'initial');
                 });
@@ -127,15 +135,10 @@ export default header;
                 $('#login .hint2').css('display', 'initial');
             };
         });
-        $('#login').on('click', '.bt4', e => {//注册成功
-            if ($(this).val() === iphoneNunber) {
-                return false
-            }
-            clearInterval(timer);
-            $('#login').find('[style]').removeAttr('style');
-            $('#login .option').removeClass('active');//清除行内样式
-            $('#login').stop(true, true).fadeOut(400, () => { $('#header .mengBan').css("display", "none") });
-            console.log($('#login').find('[style]'));
+        //注册成功
+        $('#login').on('click', '.bt4', e => {
+            clearInterval(timer);//清除计时器
+            delLogin();//清除登录模块
         })
     });
 };
@@ -156,18 +159,18 @@ function widthAuto(dom) {//脱标的父元素宽度度自适应
     });
     $(dom).width(width);
 };
-function loginInit() {//初始化登录框
+function loginInit() {//初始化登录框状态
     $('#login .option').eq(0).addClass('active');
     $('#login .con').eq(0).css('display', 'block');
     $('#login .con input[type=checkbox]').prop('checked', true);
-    $('#login input[type=text]').val('');
-    $('#login .selecter .number').text($('#login .selecter li b').eq(0).text());
-    hintInit($('#login hint'));
-    $('#login .bt1').removeClass('active1');
-    $('#login .bt2').removeClass('active2');
-    $('#login .bt1').val('获取验证码');
+    $('#login .selecter .number').text($('#login .selecter b').eq(0).text())
 };
-
+function delLogin(){//清除登录模块
+    $('#login').stop(true, true).fadeOut(400, () => {
+        $('#header .mengBan').css("display", "none");
+        $('#login').empty();//清除登录模块
+    });
+}
 function hintInit(hint) {//初始化验证栏状态
     $(hint).text('！');
     $(hint).css("borderWidth", "2");
@@ -182,5 +185,85 @@ function randomWord(num, len) {// 随机产生num位验证码,len为取值范围
     };
     return str;
 };
-
-
+// login html结构
+var loginDom = `<a class="del" href="###">×</a>
+<div class="title">
+    <a class="option" href="###">扫码登录</a>
+    <a class="option" href="###">账号登录</a>
+</div>
+<div class="con">
+    <p>
+        <a target="_blank" href="https://v.sf-express.com">我是月结企业用户</a>
+    </p>
+    <h4>登录后即可免验证快速下单与查件</h4>
+    <img src="./img/index_img/img3.png" alt="">
+    <h5>请使用微信扫描二维码</h5>
+    <h4>
+        <input type="checkbox" id="zidong">
+        <label for="zidong">自动登录</label>
+    </h4>
+</div>
+<div class="con">
+    <p class="p3">登录后即可免验证快速下单与查件</p>
+    <div class="form">
+        <div class="form_group">
+            <div class="selecter">
+                <span class="number"></span>
+                <i class="arrows "></i>
+                <ul class="list">
+                    <li><span>中国内地</span><b>+86</b></li>
+                    <li><span>中国香港</span><b>+852</b></li>
+                    <li><span>中国澳门</span><b>+853</b></li>
+                    <li><span>中国台湾</span><b>+886</b></li>
+                </ul>
+            </div>
+            <input type="text" class="txt" placeholder="请输入手机号">
+            <i class="hint">!</i>
+        </div>
+        <div class="form_group">
+            <input type="text" class="txt t2" placeholder="请输入验证码">
+            <input type="button" class="btn bt1" value="获取验证码">
+            <span class="hint2">输入有误</span>
+        </div>
+        <p class="p4">未注册用户登录默认注册顺丰会员</p>
+        <h4>
+            <input type="checkbox" id="zidong2">
+            <label for="zidong2">自动登录</label>
+        </h4>
+        <input type="button" class="btn bt2" value="快速登录">
+        <a class="big" target="_blank" href="https://v.sf-express.com">我是月结企业用户</a>
+    </div>
+</div>
+<p class="p2">
+    <span>
+        登录即代表您已经同意<a href="https://www.sf-express.com/cn/sc/Privacy_Policy/">《顺丰速运隐私政策》</a>
+    </span>
+</p>
+<div class="son w-position">
+    <a class="del2" href="###">×</a>
+    <strong>图形验证码</strong>
+    <div class="code" style="user-select: none;"></div>
+    <input type="text" class="txt t3" placeholder="请输入图片中验证码">
+    <input type="button" class="btn bt3 active2" value="确定">
+    <p class="p5">温馨提示：同一手机号码，每天限收15条验证短信</p>
+    <div class="hide w-position">请输入正确的验证码</div>
+</div>
+<div class="clause w-position">
+    <a class="del2" href="###">×</a>
+    <strong>服务条款及隐私条款</strong>
+    <div class="conText">
+        <p>
+            在您注册成为顺丰速运会员的过程中，您需要通过点击同意的形式在线签署 《顺丰速运服务条款》、
+            《顺丰速运隐私政策》，请您务必仔细阅读、充分理解条款内容后再点击同意（尤其是以粗体并下划线标识的条款，因为这些条款可能会明确您应履行的义务或对您的权利有所限制）。
+        </p>
+        <p>
+            请您注意：如果您不同意上述服务条款、隐私政策或其中任何约定，请您停止注册。如您阅读并点击同意即表示您已充分阅读、理解并接受其全部内容，并表明您也同意顺丰速运可以依据以上隐私政策来处理您的个人信息。如您对以上服务条款、隐私政策有任何疑问，您可联系顺丰速运客服。
+        </p>
+        <p>
+            点击同意即表示您已阅读并同意<a target="_blank" href="https://www.sf-express.com/cn/sc/other/terms_service/index.html">《顺丰速运服务条款》</a>、 <a target="_blank" href="https://www.sf-express.com/cn/sc/Privacy_Policy/index.html">《顺丰速运隐私政策》</a>
+        </p>
+    </div>
+    <input type="button" class="btn bt4 active2" value="同意条款并注册会员">
+</div>
+<div class="mengBan2"></div>
+`
