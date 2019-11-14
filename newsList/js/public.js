62947cd84a654dcd313cdf0ba0d04a329849c4f2
crk14 <crk14@qq.com>
2019-11-14 15:55:20 +0800

    11

--- newsList/js/public.js
+++ newsList/js/public.js
@@ -15,7 +15,11 @@ import footer from '../../js/footer.js'
 footer('../common/footer.html')
 // $('#login').click(function(){
 //     console.log(6);
-    
+var timer = setTimeout(function(){
+    if($('.nav>ul>li>a').attr('href').indexOf('./') >= 0){
+        $('.nav>ul>li>a').attr('href','../index.html')
+    }
+},1000)  
 // })
 // $('#login .con img').attr('src','../../img/index_img/img3.png');
 
